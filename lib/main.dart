b89d4a7a98def8afbc2f9f194616e0cd510e19a8
TEJASKUMAR-REDDY-J <144707021+TEJASKUMAR-REDDY-J@users.noreply.github.com>
2023-09-28 14:19:17 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 // ignore: unused_import
 import 'package:google_fonts/google_fonts.dart';
 import 'homepage.dart'; // Import your homepage widget
-// ignore: unused_import
-import 'resultpage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,8 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'BMI',
       theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 7, 54, 124)),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 23, 78, 26)),
         useMaterial3: true,
       ),
       home: const Homepage(),
