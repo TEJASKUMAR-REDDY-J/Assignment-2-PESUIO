3ad13f1a13ba781b6fa48c7c3bf02a18995f0f40
TEJASKUMAR-REDDY-J <144707021+TEJASKUMAR-REDDY-J@users.noreply.github.com>
2023-10-04 19:29:03 +0530

    resultpage.dart

--- lib/resultpage.dart
+++ lib/resultpage.dart
@@ -1,43 +1,42 @@
 import 'package:flutter/material.dart';
+// ignore: unused_import
 import 'package:google_fonts/google_fonts.dart';
 // ignore: unused_import
 import 'dart:math';
+// ignore: unused_import
+import 'homepage.dart';
 
 class ResultPage extends StatelessWidget {
   final double finalBmi;
 
-  const ResultPage({super.key, required this.finalBmi});
+  ResultPage({required this.finalBmi});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xffb2d8d8),
       appBar: AppBar(
-        backgroundColor: const Color(0xff008080),
-        title: Text(
-          "BMI Result",
-          style: GoogleFonts.poppins(
-            textStyle: const TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
+        title: Text("BMI Result"),
+      ),
+      backgroundColor: Color.fromARGB(255, 173, 216, 230), // Light blue background color
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Your BMI is:",
+              style: TextStyle(fontSize: 24, color: Colors.blue), // Dark blue text color
             ),
-          ),
+            Text(
+              finalBmi.toStringAsFixed(1), // Display BMI with one decimal place
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue, // Dark blue text color
+              ),
+            ),
+          ],
         ),
       ),
-      body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Text(
-            "Your BMI is",
-            style: GoogleFonts.poppins(
-              textStyle: const TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-              ),
-            )
-            )
-            ]
-            )
-            )
-            );
+    );
   }
-  }
+}
