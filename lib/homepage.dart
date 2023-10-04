62a61357ba5dbb2d78fdd1adf17466704cec31d6
TEJASKUMAR-REDDY-J <144707021+TEJASKUMAR-REDDY-J@users.noreply.github.com>
2023-10-04 19:28:19 +0530

    homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,53 +1,34 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'resultpage.dart'; // Import your resultpage widget
 
-class Homepage extends StatefulWidget {
-  const Homepage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<Homepage> createState() => _HomepageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _HomepageState extends State<Homepage> {
-  final TextEditingController _weight = TextEditingController();
-  final TextEditingController _height = TextEditingController();
-  double finalBmi = 0;
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _weightController = TextEditingController();
+  final TextEditingController _heightController = TextEditingController();
 
-  void bmiCalculator() {
-    int var1 = int.tryParse(_weight.text) ?? 0;
-    int var2 = int.tryParse(_height.text) ?? 0;
+  double? finalBmi;
 
-    if (var1 > 0 && var2 > 0) {
-      setState(() {
-        finalBmi = (var1 / pow((var2 / 100), 2));
-      });
+  void _calculateBMI() {
+    double weight = double.tryParse(_weightController.text) ?? 0.0;
+    double height = double.tryParse(_heightController.text) ?? 0.0;
 
+    if (weight > 0 && height > 0) {
+      // BMI formula: BMI = weight (kg) / (height (m) * height (m))
+      double bmi = weight / ((height / 100) * (height / 100));
+
+      // Navigate to the result page with the calculated BMI
       Navigator.push(
-        context,
+        context as BuildContext,
         MaterialPageRoute(
-          builder: (context) => ResultPage(finalBmi: finalBmi),
+          builder: (context) => ResultPage(finalBmi: bmi),
         ),
-      );
-    } else {
-      showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: const Text("Invalid Input"),
-            content: const Text("Please enter valid weight and height values."),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text("OK"),
-              ),
-            ],
-          );
-        },
       );
     }
   }
@@ -55,88 +36,119 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 6, 215, 230),
+      backgroundColor: Color.fromARGB(255, 103, 181, 232),
       appBar: AppBar(
         title: Text(
-          'BMI',
+          "BMI",
           style: GoogleFonts.poppins(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
           ),
         ),
       ),
       body: Center(
         child: Column(
           children: [
-            Text(
-              "Enter Height (in m)",
-              style: GoogleFonts.poppins(
-                textStyle: const TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
+            const SizedBox(
+              height: 50,
             ),
-            const SizedBox(height: 20),
             Container(
-              width: 350,
               height: 150,
+              width: 150,
               decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 10, 84, 97),
                 borderRadius: BorderRadius.circular(20),
+                color: Color.fromARGB(255, 49, 234, 169),
               ),
-              child: TextField(
-                controller: _height,
-                keyboardType: TextInputType.number,
-                textAlign: TextAlign.center,
-                decoration: const InputDecoration(hintText: "Enter height"),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Enter Weight (in kg)",
+                    style: GoogleFonts.poppins(
+                      textStyle: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: 100,
+                    color: Colors.white.withOpacity(0.8),
+                    child: TextField(
+                      controller: _weightController,
+                      keyboardType: TextInputType.number,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        hintText: "Enter Weight",
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Text(
-              "Enter weight (in kg)",
-              style: GoogleFonts.poppins(
-                textStyle: const TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
+            SizedBox(
+              height: 30,
             ),
-            const SizedBox(height: 20),
             Container(
+              height: 200,
               width: 350,
-              height: 150,
               decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 10, 84, 97),
                 borderRadius: BorderRadius.circular(20),
+                color: Color.fromARGB(255, 36, 218, 181),
               ),
-              child: TextField(
-                controller: _weight,
-                keyboardType: TextInputType.number,
-                textAlign: TextAlign.center,
-                decoration: const InputDecoration(hintText: "Enter weight"),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Enter Height (in cm)",
+                    style: GoogleFonts.poppins(
+                      textStyle: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 350,
+                    color: Colors.white.withOpacity(0.8),
+                    child: TextField(
+                      controller: _heightController,
+                      keyboardType: TextInputType.number,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        hintText: "Enter Height",
+                      ),
+                    ),
+                  ),
+                ],
               ),
+            ),
+            SizedBox(
+              height: 15,
             ),
             ElevatedButton(
-              onPressed: () => bmiCalculator(),
+              onPressed: _calculateBMI,
               child: Padding(
                 padding: const EdgeInsets.only(
-                  left: 12,
+                  left: 12.0,
                   right: 12.0,
                   top: 8.0,
                   bottom: 8.0,
                 ),
                 child: Text(
-                  "Compute",
-                  style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
+                  "Compute BMI",
                 ),
               ),
             ),
